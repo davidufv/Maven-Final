3d7ce6eb22c39d638e4656a1787dd9b47bc0967e
davidufv <davidsv92@gmail.com>
2019-01-08 22:34:50 +0100

    Update Transaccion.java

--- vaadin-inventario/src/main/java/vaadin/Dao/Dominio/Transaccion.java
+++ vaadin-inventario/src/main/java/vaadin/Dao/Dominio/Transaccion.java
@@ -8,14 +8,26 @@ public class Transaccion {
 	private Date fecha;
 	private Integer codigo;
 	private Double cantidadT;
+	private Double costesTotales = 0.0;
 	
-	
+	public Transaccion(Double costesTotales) {
+	// TODO Auto-generated constructor stub
+	this.costesTotales = costesTotales;
+}
 	
 	public Transaccion(Double cantidadT, Integer codigo,Date fecha ) {
 		super();
 		this.fecha = fecha;
 		this.codigo = codigo;
 		this.cantidadT = cantidadT;
+	}
+
+	public Double getCostesTotales() {
+		return costesTotales;
+	}
+
+	public void setCostesTotales(Double costesTotales) {
+		this.costesTotales = costesTotales;
 	}
 
 	public Double getCantidadT() {
